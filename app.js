cad6fe4c8b8a866356dcc00f02dc803059a14a6d
Shreyas Gore <shreyashgore123@gmail.com>
2023-07-01 13:40:12 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading1=React.createElement("h1",{id:"title"},"this is heading h1")
+const heading2=React.createElement("h2",{id:"title"},"this is heading h2")
+const container=React.createElement("div",{id:"container"},[heading1,heading2,"this is div from react"])
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(container)
