e2311a9946d41de05a32fa1306365ebefd3adc0c
Shreyas Gore <shreyashgore123@gmail.com>
2023-07-02 10:27:29 +0530

    this is 2nd class of parcel

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1=React.createElement("h1",{id:"title"},"this is heading h1")
 const heading2=React.createElement("h2",{id:"title"},"this is heading h2")
 const container=React.createElement("div",{id:"container"},[heading1,heading2,"this is div from react"])
@@ -5,3 +8,4 @@ const container=React.createElement("div",{id:"container"},[heading1,heading2,"t
 const root=ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(container)
+
